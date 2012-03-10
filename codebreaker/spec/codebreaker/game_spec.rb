56d41d4f6592e939d3c6b1eac9593409c6c194a2
Rod Knowlton <codelahoma@gmail.com>
2012-03-10 17:57:33 -0600

    one step into submit guess scenarios

---
+++ codebreaker/spec/codebreaker/game_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+module Codebreaker
+  describe Game do
+    describe "#start" do
+      let(:output) { double('output').as_null_object }
+      let(:game)   { Game.new(output) }
+
+      it "sends a welcome message" do
+        output.should_receive(:puts).with('Welcome to Codebreaker!')
+
+        game.start('1234')
+      end
+
+      it "prompts for the first guess" do
+        output.should_receive(:puts).with('Enter guess:')
+
+        game.start('1234')
+      end
+    end
+  end
+end
